4e6274f736441f1173b2d39349e58af03b98cf46
jesusextream <jesus.diaz19853@gmail.com>
2018-01-17 05:29:56 -0400

    ok

--- dts/facturas/download.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$enlace = "files/productsQBO.xlsx";
-header ("Content-Disposition: attachment; filename=productsQBO.xlsx");
-header ("Content-Type: application/octet-stream");
-header ("Content-Length: ".filesize($enlace));
-readfile($enlace)
-
-?>
